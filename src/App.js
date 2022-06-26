88b9867297d265d39d6c6ca68b58fab2bdc25221
angyein <angyein@angyeinui-MacBookPro.local>
2022-06-26 16:36:04 +0900

    [feature] Add react-tabs
    - react-tabs 적용.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
-function App() {
-  return "hello";
-}
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
+const App = () => {
+  return (
+    <Tabs>
+      <TabList>
+        <Tab>가계부</Tab>
+        <Tab>주식</Tab>
+      </TabList>
+
+      <TabPanel>
+        {/* TODO: 가계부 화면 */}
+        <h2>Any content 1</h2>
+      </TabPanel>
+      <TabPanel>
+        {/* TODO: 주식 화면 */}
+        <h2>Any content 2</h2>
+      </TabPanel>
+    </Tabs>
+  );
+};
 
 export default App;
